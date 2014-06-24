b378b393f9e69322f683fc58fe81223faaba8086
hqukai <hqukai@gmail.com>
2014-06-25 00:02:59 +0800

    testsql

---
+++ testsql/src/com.hqukai.testsql/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/Go-SQL-Driver/MySQL"
+	//"time"
+)
+
+func main() {
+	db, err := sql.Open("mysql", " root:root@/test?charset=utf8")
+	checkErr(err)
+
+	stmt, err := db.Prepare("INSERT userinfo SET username=?,departname=?,created=?")
+	checkErr(err)
+
+	res, err := stmt.Exec("hankai", "研发部", "2013-6-23")
+	checkErr(err)
+
+	id, err := res.LastInsertId()
+	checkErr(err)
+
+	fmt.Println(id)
+	//更新数据
+	stmt, err := db.Prepare("update userinfo set username = ? where uid = ?")
+	checkErr(err)
+
+	res, err := db.Exec("hankai1", id)
+	checkErr(err)
+
+	affect, err := res.RowsAffected()
+	checkErr(err)
+
+	fmt.Println(affect)
+
+}
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
